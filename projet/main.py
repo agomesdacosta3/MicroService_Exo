597690283b64b98a4c6fc8d8cb7f4ce4ae7812e6
agomesdacosta3 <caps92@live.fr>
2023-10-19 15:10:44 +0200

    add customer

--- projet/main.py
+++ projet/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
+
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
+
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
@@ -20,6 +22,28 @@ async def read_root(request: Request):
 @app.get('/add_customer')
 async def add_customer(request: Request):
     return templates.TemplateResponse("add_customer.html", {"request": request})
+
+@app.post('/add_customer_db')
+async def add_customer_db(
+    customerid: str = Form(...),
+    companyname: str = Form(...),
+    contactname: str = Form(...)
+):
+    session = Session()
+
+    # Insérer le nouveau client dans la base de données
+    insert_query = text("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (:customerid, :companyname, :contactname)")
+    try:
+        session.execute(insert_query, {"customerid": customerid, "companyname": companyname, "contactname": contactname})
+        session.commit()
+        message = "Customer added successfully."
+    except Exception as e:
+        message = f"Error adding customer"
+
+    # Créer une réponse JSON avec le message
+    response_data = {"message": message}
+    return JSONResponse(content=response_data)
+    
 
 @app.get('/get_customer')
 async def get_customer(request: Request):
@@ -47,6 +71,6 @@ async def search_customer(request: Request, contact_name: str):
 @app.get('/get_all_customer')
 async def get_all_customer(request: Request):
     session = Session()
-    result = session.execute(text('SELECT customer_id, company_name, contact_name FROM customers LIMIT 10'))
+    result = session.execute(text('SELECT customer_id, company_name, contact_name FROM customers LIMIT 20'))
     customers = [dict(customerid=row[0], companyname=row[1], contactname=row[2]) for row in result]
     return templates.TemplateResponse("get_all_customer.html", {"request": request, "customers": customers})
